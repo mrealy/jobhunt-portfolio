18936264f99b150a2143acdb88b56d4120540097
Michael Realy <mjrealy@gmail.com>
2017-11-09 13:04:08 -0600

    Created package.json file and installed dependencies

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//create static public folder
+app.use(express.static("./public"));
+
+// react router route
+// app.get("*", function(req, res) {
+//     res.sendFile(__dirname + "/public/index.html");
+// });
+
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
