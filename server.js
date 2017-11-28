8b4cd489f710aa4489c8f8ef76e5b6f01631676c
Michael Realy <mjrealy@gmail.com>
2017-11-28 12:25:58 -0600

    added nocache function/middleware to app.get route

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 //create static public folder
 app.use(express.static("./public"));
 
+function nocache(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+  }
+
 // react router route
-app.get("*", function(req, res) {
+app.get("*", nocache, function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
