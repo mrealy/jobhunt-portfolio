310e37bebd692fd8bf1468437b8bc65e5d8ddb10
Michael Realy <mjrealy@gmail.com>
2017-11-27 22:16:31 -0600

    spelling correction on about.js file

--- app/components/children/About.js
+++ app/components/children/About.js
@@ -5,7 +5,7 @@ const About = () => (
         <h2> About me </h2>
         <img className="img-margin" src="/img/headshot-100x140.jpg" alt="headshot" />
         <img src="/img/cellist.jpg" alt="cellist" />
-        <p id="about-text"> I'm a ulti-instrumentalist, <a href="https://soundcloud.com/mike-realy">composer</a>, 
+        <p id="about-text"> I'm a multi-instrumentalist, <a href="https://soundcloud.com/mike-realy">composer</a>, 
         songwriter, and private music instructor with a BA in music composition from Columbia College 
         '13.  As a recent graduate of the Northwestern Coding bootcamp, I'm excited to shift my career
         towards web development. </p>
