d4710fb7ce94a6fa9ccd38730109c1f18e57923b
Michael Realy <mjrealy@gmail.com>
2017-12-05 12:57:48 -0600

    made css tweaks to about.js component elemnts. Changed the 'about me' text.

--- app/components/children/About.js
+++ app/components/children/About.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const About = () => (
     <div className="container">
-        <h2> About me </h2>
-        <img className="img-margin" src="/img/headshot-100x140.jpg" alt="headshot" />
-        <img src="/img/cellist.jpg" alt="cellist" />
-        <p id="about-text"> I'm a multi-instrumentalist, <a href="https://soundcloud.com/mike-realy">composer</a>, 
-        songwriter, and private music instructor with a BA in music composition from Columbia College 
-        '13.  As a recent graduate of the Northwestern Coding bootcamp, I'm excited to shift my career
-        towards web development. </p>
+        <div className="about-tile">
+            <h2> About me </h2>
+            <img className="cellist-scale" src="/img/cellist.jpg" alt="cellist" />
+            <p className="about-text"> I'm a multi-instrumentalist, <a href="https://soundcloud.com/mike-realy">composer</a>, 
+            songwriter, and private music instructor.</p>
+            <p className="about-text"> I grew up building my own computers and playing video games.</p>
+            <p className="about-text"> I love comedy and making people laugh/smile.</p>
+            <p className="about-text"> I listen to podcasts daily, my favs: <a href="http://billburr.com/podcast-2/">Bill Burr's Monday Morning Podcast</a>, <a href="http://podcasts.joerogan.net/">The Joe Rogan Experience</a>, <a href="https://www.samharris.org/podcast">Waking up with Sam Harris</a>, <a href="https://www.codenewbie.org/podcast">CodeNewbie</a>, & <a href="https://learntocodewith.me/podcast/">Learn to Code with Me</a>.</p>
+            <p className="about-text"> I'm a great cook (especially grilling), bartender, and host. So basically, I throw bitchin' BBQs!</p>            
+        </div>
     </div>
 )
 
