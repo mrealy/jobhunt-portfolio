e99e393d6939c2264074b6212ad6f49082343de4
Michael Realy <mjrealy@gmail.com>
2017-12-05 22:02:46 -0600

    removed row class

--- app/components/children/Resume.js
+++ app/components/children/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Resume extends React.Component {
     render() {
         return (
-            <div className="embed-responsive row">
+            <div className="embed-responsive">
                 <embed id="pdf" src="/Resume.pdf" type="application/pdf"></embed>
             </div>
         )
