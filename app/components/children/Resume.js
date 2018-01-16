fe3738cc3f1ded9aa87ed3eb1d216be484ca8e09
Michael Realy <mjrealy@gmail.com>
2018-01-15 19:56:11 -0600

    Added style to download-resume button

--- app/components/children/Resume.js
+++ app/components/children/Resume.js
@@ -4,7 +4,7 @@ class Resume extends React.Component {
     render() {
         return (
             <div>
-                <a href="/Resume.pdf"> Download <span className="glyphicon glyphicon-download-alt" /></a>                        
+                <a href="/Resume.pdf" id="download-resume"> Download <span className="glyphicon glyphicon-download-alt" /></a>                        
                 <div className="embed-responsive">
                     <embed id="pdf" src="/Resume.pdf" type="application/pdf"></embed>
                 </div>
