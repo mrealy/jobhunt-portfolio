9e79b4cf6ac27d62018ad39847c24561146e8237
Michael Realy <mjrealy@gmail.com>
2018-01-15 19:54:35 -0600

    Made changes to bio wording

--- app/components/children/About.js
+++ app/components/children/About.js
@@ -5,12 +5,10 @@ const About = () => (
         <div className="about-tile">
             <h2> About me </h2>
             <img className="cellist-scale" src="/img/cellist.jpg" alt="cellist" />
-            <p className="about-text"> I'm a multi-instrumentalist, <a href="https://soundcloud.com/mike-realy">composer</a>, 
-            songwriter, and private music instructor.</p>
-            <p className="about-text"> I grew up building my own computers and playing video games.</p>
-            <p className="about-text"> I love comedy and making people laugh/smile.</p>
-            <p className="about-text"> I listen to podcasts daily, my favs: <a href="http://billburr.com/podcast-2/">Bill Burr's Monday Morning Podcast</a>, <a href="http://podcasts.joerogan.net/">The Joe Rogan Experience</a>, <a href="https://www.samharris.org/podcast">Waking up with Sam Harris</a>, <a href="https://www.codenewbie.org/podcast">CodeNewbie</a>, & <a href="https://learntocodewith.me/podcast/">Learn to Code with Me</a>.</p>
-            <p className="about-text"> I love to cook food and will use the grill well into the winter.</p>            
+            <p className="about-text"> In addition to being a developer, I'm a <a href="https://soundcloud.com/mike-realy" target="blank">musician </a>
+            and private music instructor.  I'm tuning up my database skills to develop a large-scale website that will assist in lesson-planning.</p>
+            <p className="about-text"> I listen to podcasts daily, my favs: <a href="https://www.codenewbie.org/podcast" target="blank">CodeNewbie</a>, <a href="https://spec.fm/podcasts/developer-tea" target="blank">Developer Tea</a>, <a href="http://frontendhappyhour.com/" target="blank">Front End Happy Hour</a>, <a href="http://www.fullstackradio.com/" target="blank">Full Stack Radio</a>, & <a href="https://learntocodewith.me/podcast/" target="blank">Learn to Code with Me</a>.</p>            
+            <p className="about-text"> I grew up building my own computers and playing video games. Before getting into the web, I wanted to be a game coder.</p>            
         </div>
     </div>
 )
