568e07525b0af112214089b3f2d64e1df0aacfb0
Michael Realy <mjrealy@gmail.com>
2018-03-21 12:47:20 -0500

    Added object for mongo good news scraper project.  Also removed contributors' githubs.

--- app/API/projects.js
+++ app/API/projects.js
@@ -1,6 +1,7 @@
 var projects = [
     {
         id: "tender",
+        type: "group",
         title: "tender",
         main_tools: ["HTML", "jQuery", "Spoonacular API"],
         role: "Front End",
@@ -11,13 +12,13 @@ var projects = [
         description: "Recipe browsing app with the interface of a dating app.  Comically inspired by tinder, this app allows users to quickly browse through multiple recipes using the Seatgeek API.  They can set filters for what to search for, and browse multiple recipes side-by-side, and then route to the full recipe page when they've made a selection.",
         contribution: ["Project conception", "Project lead", "Wireframing & HTML", "jQuery", "Spoonacular API data extraction"],        
         screenshots: ["/img/tender-1.PNG", "/img/tender-2.PNG", "/img/tender-3.PNG"],
-        contributors: ["Laura McGinn", "Jeannine Hall", "Paul Sulikowski"],
-        contributor_githubs: ["https://github.com/LauraMcG", "https://github.com/jlh974", "#"]
+        contributors: ["Laura McGinn", "Jeannine Hall", "Paul Sulikowski"]
     },
     {
         id: "who",
+        type: "group",
         title: "who ARE you?",
-        main_tools: ["node/express", "handlebars", "Sequelize"],
+        main_tools: ["Node/Express", "handlebars", "Sequelize"],
         role: "Full Stack",
         short: "Questions game for getting to know people (CRUD app)",
         github: "https://github.com/thensle/whoAreYou_Game",
@@ -26,13 +27,13 @@ var projects = [
         description: "Get to know you card game with safe for work and not safe work questions.  Ability to create new cards, delete cards, or edit them.",
         contribution: ["Express GET/POST Routes", "Sequelize models", "Handlebars templating"],
         screenshots: ["/img/whoAREyou-1.PNG", "/img/whoAREyou-2.PNG", "/img/whoAREyou-3.PNG"],
-        contributors: ["Julie Fisher", "Liam Fox", "Tara Hensle", "Chris Leeson"],
-        contributor_githubs: ["https://github.com/jefisher1991", "https://github.com/FoxMessenger", "https://github.com/thensle", "https://github.com/FourEyeSamurai"]      
+        contributors: ["Julie Fisher", "Liam Fox", "Tara Hensle", "Chris Leeson"]
     },
     {
         id: "shuttle",
+        type: "group",
         title: "ShuttleExtravaganza!",
-        main_tools: ["node/express", "React", "Mongoose"],
+        main_tools: ["Node/Express", "React", "Mongoose"],
         role: "Full Stack",
         short: "App for finding local events and connecting with other event attendees",
         github: "https://github.com/linhng15/Extravaganza",
@@ -41,13 +42,13 @@ var projects = [
         description: "App for finding and saving upcoming events that helps users find other app users near them that are also interested in the same events.  Find a ride or a friend!",
         contribution: ["Project conception", "Project lead", "React Components", "Mongoose modeling", "Monoose GET/POST requests"],
         screenshots: ["/img/Extravaganza-1.PNG", "/img/Extravaganza-2.PNG", "/img/Extravaganza-3.PNG"],
-        contributors: ["Caryn Carter", "Jared Moscrip", "Linh Nguyen"],
-        contributor_githubs: ["https://github.com/Caryndcarter", "https://github.com/1jared123", "https://github.com/linhng15"]
+        contributors: ["Caryn Carter", "Jared Moscrip", "Linh Nguyen"
     },
     {
         id: "NYT",
+        type: "solo",
         title: "NYT Article Search",
-        main_tools: ["React", "Mongo DB", "Node/express"],
+        main_tools: ["React", "Mongoose", "Node/Express"],
         role: "Full Stack",
         short: "MERN app for searching and saving articles through the New York Times API.",
         github: "https://github.com/mrealy/NYT-react",
@@ -56,8 +57,22 @@ var projects = [
         description: "Search for New York Times articles by making axios get requests to the NYT api.  Using a react/bootstrap front end, lists articles you can then view or save to the database using mongoose.",
         contribution: ["Sole Contributor"],        
         screenshots: ["/img/NYTreact-1.png", "/img/NYTreact-2.png", "/img/NYTreact-3.png"],
-        contributors: ["Sole Contributor"],
-        contributor_githubs: ["https://github.com/mrealy"]
+        contributors: ["Sole Contributor"]
+    },
+    {
+        id: "scraper",
+        type: "solo",
+        title: "Good News Scraper",
+        main_tools: ["cheerio", "Node/Mongoose", "handlebars/jQuery"],
+        role: "Full Stack",
+        short: "Web app that scrapes news articles from Good News Newtork that can be viewed, saved, and commented on.",
+        github: "https://github.com/mrealy/mongo-scraper",
+        heroku: "https://good-news-scraper.herokuapp.com",
+        tools: "jQuery, handlebars, CSS, Node, Express, Mongoose, cheerio",        
+        description: "Web app that scrapes uplifting news articles from goodnewsnetwork.org and displays them on the front end for viewing or saving for later using handlebars templates. Saved articles can be reviewed and commented upon for discussion.",
+        contribution: ["Sole Contributor"],        
+        screenshots: ["/img/scraper-1.png", "/img/scraper-2.png", "/img/scraper-3.png"],
+        contributors: ["Sole Contributor"]
     }
 ]
 
